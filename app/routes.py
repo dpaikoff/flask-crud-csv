3be59cca36d5cb883cc48fa4cbcf3c31755d1a7d
Daniel Paikoff <dpaikoff@gmail.com>
2018-06-08 13:57:07 -0700

    update route error handling for internal server error

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from io import StringIO
-from flask import request, make_response, render_template, redirect
+from flask_api import status
+from flask import request, make_response, redirect
 
 from app import app, db
 
@@ -8,23 +9,30 @@ from app import app, db
 @app.route('/upload_file', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['file']
-        df = pd.read_csv(f.stream)
-        df.to_sql('items', con=db.engine, if_exists='replace', index=False)
-        # return render_template('admin/upload_success.html')
-        return redirect('admin/upload_success')
+        try:
+            f = request.files['file']
+            df = pd.read_csv(f.stream)
+            df.to_sql('items', con=db.engine, if_exists='replace', index=False)
+            return redirect('admin/upload_success')
+        except Exception as e:
+            content = {'Internal Server Error': e.message}
+            return content, status.HTTP_500_INTERNAL_SERVER_ERROR
 
 
 @app.route('/download_file', methods = ['GET', 'POST'])
 def download_file():
     if request.method == 'POST':
-        df = pd.read_sql('items', con=db.engine)
-        buffer = StringIO()
-        df.to_csv(buffer, index=False)
-        buffer.seek(0)
-        response = make_response(buffer.getvalue())
-        cd = 'attachment; filename=items.csv'
-        response.headers['Content-Disposition'] = cd
-        response.mimetype='text/csv'
-        return response
+        try:
+            df = pd.read_sql('items', con=db.engine)
+            buffer = StringIO()
+            df.to_csv(buffer, index=False)
+            buffer.seek(0)
+            response = make_response(buffer.getvalue())
+            cd = 'attachment; filename=items.csv'
+            response.headers['Content-Disposition'] = cd
+            response.mimetype='text/csv'
+            return response
+        except Exception as e:
+            content = {'Internal Server Error': e.message}
+            return content, status.HTTP_500_INTERNAL_SERVER_ERROR
 
